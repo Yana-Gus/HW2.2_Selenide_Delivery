ca1d55cb2398ddd65bcb218051daa04fa3924a6a
Yana G <ivana-1@mail.ru>
2023-08-18 15:53:08 +0300

    Added .jar and test

--- src/test/java/ru/netology/service/CardDeliveryOrderTest.java
+++ src/test/java/ru/netology/service/CardDeliveryOrderTest.java
@@ -13,8 +13,7 @@ import java.time.format.DateTimeFormatter;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryOrderTest {
     private String generateDate(int addDays, String pattern) {
@@ -34,7 +33,21 @@ public class CardDeliveryOrderTest {
         $("button.button").click();
         $(".notification__content").shouldBe(visible, Duration.ofSeconds(15))
                 .shouldHave(Condition.exactText("Встреча успешно забронирована на " + bookedDate));
+    }
 
-
+    @Test
+    public void shouldEnteredTwoCharactersInTheCityField() {
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Ка");
+        $$(".menu-item__control").find(exactText("Казань")).click();
+        String bookedDate = generateDate(5, "dd.MM.yyyy");
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME));
+        $("[data-test-id=date] input").sendKeys(bookedDate);
+        $("[data-test-id=name] input").setValue("Сидорова Анна-Мария");
+        $("[data-test-id=phone] input").setValue("+71234567890");
+        $("[data-test-id=agreement]").click();
+        $("button.button").click();
+        $(".notification__content").shouldBe(visible, Duration.ofSeconds(15))
+                .shouldHave(exactText("Встреча успешно забронирована на " + bookedDate));
     }
 }
